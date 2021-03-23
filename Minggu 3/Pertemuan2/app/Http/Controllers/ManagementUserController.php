bdb089857df56e9027833559df08a8356321290d
RandyR025 <71814522+RandyR025@users.noreply.github.com>
2021-03-24 00:32:27 +0700

    Update ManagementUserController.php

--- Minggu 3/Pertemuan2/app/Http/Controllers/ManagementUserController.php
+++ Minggu 3/Pertemuan2/app/Http/Controllers/ManagementUserController.php
@@ -8,7 +8,32 @@ class ManagementUserCOntroller extends Controller
 {
     //di sini isi controller Management USer
     public function index(){
-        return "Halo ini adalah method index, dalam controller ManagementUser.";
+        //return "Halo ini adalah method index, dalam controller ManagementUser.";
+        return "Method ini nantinya akan digunakan untuk mengambil semua data user.";
+    }
+
+    public function create(){
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk menambah data user.";
+    }
+
+    public function Store(Request $request){
+        return "Method ini nantinya akan digunakan untuk menciptakan form untuk menambah data user.";
+    }
+
+    public function show(){
+        return "Method ini nantinya akan digunakan untuk mengambil satu data user dengan id=" . $id;
+    }
+
+    public function edit($id){
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk mengubah data edit dengan id=" . $id;
+    }
+
+    public function update(Request $request, $id){
+        return "Method ini nantinya akan digunakan untuk mengubah data user dengan id=" . $id;
+    }
+
+    public function destroy($id){
+        return "Method ini nantinya akan digunakan untuk menghapus data user dengan id=" . $id;
     }
 }
 
