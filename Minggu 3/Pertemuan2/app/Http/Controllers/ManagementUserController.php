0411c9fe179144255627c2854456557107a6048d
RandyR025 <71814522+RandyR025@users.noreply.github.com>
2021-03-23 23:49:15 +0700

    Create ManagementUserController.php

---
+++ Minggu 3/Pertemuan2/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManagementUserCOntroller extends Controller
+{
+    //di sini isi controller Management USer
+    public function index(){
+        return "Halo ini adalah method index, dalam controller ManagementUser.";
+    }
+}
+
+
+?>
