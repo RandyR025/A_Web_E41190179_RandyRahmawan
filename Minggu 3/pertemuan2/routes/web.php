40c9f6f0038e923b1317d8fce066ee71584027f1
RandyR025 <71814522+RandyR025@users.noreply.github.com>
2021-03-23 23:49:58 +0700

    Update web.php

--- Minggu 3/pertemuan2/routes/web.php
+++ Minggu 3/pertemuan2/routes/web.php
@@ -14,4 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('user','ManagementUserController@index');
+//Route::get('user','ManagementUserController@index');
+Route::get('user','ManagementUserController');
