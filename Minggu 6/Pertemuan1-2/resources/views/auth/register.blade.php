03a1c46f73e9af0e950aecd57c5be9ff93eb0754
RandyR025 <71814522+RandyR025@users.noreply.github.com>
2021-04-15 14:45:31 +0700

    update

--- Minggu 6/Pertemuan1-2/resources/views/auth/register.blade.php
+++ Minggu 6/Pertemuan1-2/resources/views/auth/register.blade.php
@@ -11,8 +11,23 @@
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
+
                         <div class="form-group row">
-                            <label for="usernname" class="col-md-4 col-form-label text-md-right">Username</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
 
                             <div class="col-md-6">
                                 <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
