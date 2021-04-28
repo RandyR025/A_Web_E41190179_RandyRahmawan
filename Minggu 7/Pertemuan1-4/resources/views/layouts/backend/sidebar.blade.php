efe9b4adcefff0eacfbca749823fe523a43c0281
RandyR025 <71814522+RandyR025@users.noreply.github.com>
2021-04-29 05:53:36 +0700

    update

--- Minggu 7/Pertemuan1-4/resources/views/layouts/backend/sidebar.blade.php
+++ Minggu 7/Pertemuan1-4/resources/views/layouts/backend/sidebar.blade.php
@@ -9,42 +9,11 @@
                           <span>Dashboard</span>
                       </a>
           </li>
-          <li class="sub-menu">
-            <a href="javascript:;" class="">
-                          <i class="icon_document_alt"></i>
-                          <span>Forms</span>
-                          <span class="menu-arrow arrow_carrot-right"></span>
-                      </a>
-            <ul class="sub">
-              <li><a class="" href="form_component.html">Form Elements</a></li>
-              <li><a class="" href="form_validation.html">Form Validation</a></li>
-            </ul>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;" class="">
-                          <i class="icon_desktop"></i>
-                          <span>UI Fitures</span>
-                          <span class="menu-arrow arrow_carrot-right"></span>
-                      </a>
-            <ul class="sub">
-              <li><a class="" href="general.html">Components</a></li>
-              <li><a class="" href="buttons.html">Buttons</a></li>
-              <li><a class="" href="grids.html">Grids</a></li>
-            </ul>
-          </li>
           <li>
-            <a class="" href="widgets.html">
-                          <i class="icon_genius"></i>
-                          <span>Widgets</span>
-                      </a>
-          </li>
-          <li>
-            <a class="" href="chart-chartjs.html">
-                          <i class="icon_piechart"></i>
-                          <span>Charts</span>
-
-                      </a>
-
+            <a href="index.html">
+              <i class="icon_profile"></i>
+              <span>Profile</span>
+            </a>
           </li>
 
           <li class="sub-menu">
@@ -59,7 +28,7 @@
             </ul>
           </li>
 
-          <li class="sub-menu ">
+<!--           <li class="sub-menu ">
             <a href="javascript:;" class="">
                           <i class="icon_documents_alt"></i>
                           <span>Pages</span>
@@ -72,7 +41,7 @@
               <li><a class="active" href="blank.html">Blank Page</a></li>
               <li><a class="" href="404.html">404 Error</a></li>
             </ul>
-          </li>
+          </li> -->
 
         </ul>
         <!-- sidebar menu end-->
